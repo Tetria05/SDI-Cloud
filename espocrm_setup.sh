cf04befaa96e5b426253b79f63973e87d0c33417
Tetria05 <97681882+Tetria05@users.noreply.github.com>
2024-10-08 14:15:54 +0200

    Update espocrm_setup.sh

--- espocrm_setup.sh
+++ espocrm_setup.sh
@@ -4,7 +4,7 @@
 db_name="espocrm"
 db_user="espo"
 db_pw="guus123"
-ip=$(hostname -I)
+ip=$(hostname -I | awk '{print $1}')
 
 apt update
 apt install apache2 mysql-server php8.1 libapache2-mod-php8.1 php8.1-common php8.1-curl php8.1-mysql php8.1-opcache php8.1-intl php8.1-fpm php8.1-xmlrpc php8.1-bcmath php8.1-zip php8.1-mbstring php8.1-gd php8.1-cli php8.1-xml php8.1-zip wget unzip curl -y
